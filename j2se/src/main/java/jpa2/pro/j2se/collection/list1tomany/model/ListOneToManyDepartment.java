dafcbef3d9414314b0fe9bdbd74ac9808306e7d5
Xavier Ding <finchan_ding@hotmail.com>
2019-05-27 17:13:55 +0800

    List One to Many Collection

---
+++ j2se/src/main/java/jpa2/pro/j2se/collection/list1tomany/model/ListOneToManyDepartment.java
@@ -0,0 +1,61 @@
+package jpa2.pro.j2se.collection.list1tomany.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class ListOneToManyDepartment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    @OneToMany (mappedBy = "dept")
+    @OrderBy("name ASC")
+    private List<ListOneToManyEmployee> employees;
+
+    public ListOneToManyDepartment() {
+        this.employees = new ArrayList<ListOneToManyEmployee>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<ListOneToManyEmployee> getEmployees() {
+        return employees;
+    }
+
+    public void addEmployees(ListOneToManyEmployee employee) {
+        int initialSize = employees.size();
+        if(!employees.contains(employee)){
+            for (int i=0; i < initialSize; i++) {
+                if (employees.get(i).getName().compareTo(employee.getName()) > 0) {
+                    employees.add(i, employee);
+                    break;
+                }
+            }
+            //insert into the last position
+            if(employees.size() == initialSize) {
+                employees.add(employee);
+            }
+            if(employee != null) {
+                employee.getDept().getEmployees().remove(employee);
+            }
+            employee.setDept(this);
+        }
+    }
+}
